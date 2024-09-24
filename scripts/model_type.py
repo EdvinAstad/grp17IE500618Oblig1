5530464962dce8b6e4540c070b6b0c2f87f31e0a
Edvin Astad <Edvin.1996@hotmail.com>
2024-09-24 23:49:20 +0200

    Final commit

--- scripts/model_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-
-class ModelType(Enum):
-    LINEAR_REGRESSION = LinearRegression
-    RANDOM_FOREST_REGRESSION = RandomForestRegressor
